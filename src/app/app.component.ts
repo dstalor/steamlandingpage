98028119dc7cd26c7a48be195b91bcbb3adba068
Daniel Tal-Or <talor.daniel@gmail.com>
2022-11-11 12:09:11 +0200

    Try to fix api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject, take } from 'rxjs';
+import { ReplaySubject, Subject, take } from 'rxjs';
 
 const key = '366A3809CC9855BE0B939783903DBD5A';
 const api = {
@@ -16,44 +16,48 @@ const api = {
 })
 export class AppComponent {
   title = 'steamlandingpage';
-  userName = new Subject<string>();
-  userId = new Subject<string>();
-  gameData = new Subject();
+  userName$ = new ReplaySubject<string>();
+  _userName = '';
+  userId$ = new ReplaySubject<string>();
+  _userId = '';
+  gameData = new ReplaySubject();
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
       console.log(params);
-      this.userId.next(params['id']);
-      this.userName.next(params['name']);
+      this.userId$.next(params['id']);
+      this.userName$.next(params['name']);
+    });
 
-      if (!params['id']) {
+    this.userName$.subscribe((name) => {
+      this._userName = name;
+      if (!this._userId) {
         this.http
           .get<any>(
-            `https://api.steampowered.com/ISteamUser/${api.vanityURL}`,
-            { params: { key, vanityurl: params['name'] } }
+            `//api.steampowered.com/ISteamUser/${api.vanityURL}`,
+            { params: { key, vanityurl: name } }
           )
           .pipe(take(1))
           .subscribe((data) => {
             console.log(data);
-            this.userId.next(data.response?.steamid);
+            this.userId$.next(data.response?.steamid);
           });
       }
     });
-
-    this.userId.subscribe((id) => {
+    this.userId$.subscribe((id) => {
+      this._userId = id;
       this.http
-      .get<any>(
-        `https://api.steampowered.com/ISteamUser/${api.playerSummaries}`,
-        { params: { key, steamids: id } }
-      )
-      .pipe(take(1))
-      .subscribe((data) => {
-        console.log(data);
-        this.gameData.next(data.response?.players[0]);
-      });
-
-    })
+        .get<any>(
+          `//api.steampowered.com/ISteamUser/${api.playerSummaries}`,
+          { params: { key, steamids: id } }
+        )
+        .pipe(take(1))
+        .subscribe((data) => {
+          console.log(data);
+          this.gameData.next(data.response?.players[0]);
+        });
+    });
   }
 }
